a5d1759141a5d60f3fe9e87e82b7b86e8e7aeeb7
minaxivaland <minaxi_valand@hotmail.comgit config --global user.email minaxi_valand@hotmail.com>
2022-04-26 21:51:10 +0100

    Do While Homework

---
+++ EevenOdd.java
@@ -0,0 +1,17 @@
+package DoWhile;
+
+public class EevenOdd {
+     public static void main(String args[]){
+     int n =1;
+     do{
+         if (n % 2 == 0){
+
+             System.out.println(" Even " +n );}
+         else
+             System.out.println(" Odd " +n);
+         n++;
+     }while (n <=20);
+
+
+     }
+}
